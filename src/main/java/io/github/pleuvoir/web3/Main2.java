4860a6444d61f5622c20a3c0ad3a593d3197f45d
pleuvoir <pleuvoir@foxmail.com>
2023-04-27 00:59:30 +0800

    continue

---
+++ src/main/java/io/github/pleuvoir/web3/Main2.java
@@ -0,0 +1,331 @@
+package io.github.pleuvoir.web3;
+
+import io.github.pleuvoir.web3.utils.AccountHelper;
+import java.io.File;
+import java.math.BigDecimal;
+import javafx.application.Application;
+import javafx.concurrent.ScheduledService;
+import javafx.concurrent.Task;
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.image.Image;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+import org.web3j.crypto.Credentials;
+import org.web3j.utils.Convert;
+
+public class Main2 extends Application {
+
+    private Thread currentThread; // 当前任务所在的线程
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        // 创建一个垂直布局
+        VBox root = new VBox();
+        root.setPadding(new Insets(10));
+        root.setSpacing(10);
+
+        // 创建一个水平布局用于放置按钮
+        GridPane buttonBox = new GridPane();
+        buttonBox.setHgap(10);
+        buttonBox.setVgap(10);
+
+        Label gasFeeLabel = new Label("GAS Fee: 0.0");
+        buttonBox.add(gasFeeLabel,0,0);
+
+        Button generateKeyButton = new Button("生成账户");
+        generateKeyButton.getStyleClass().add("button-raised");
+        buttonBox.add(generateKeyButton, 1, 0);
+
+        Button importKeyButton = new Button("导入私钥");
+        importKeyButton.getStyleClass().add("button-raised");
+        buttonBox.add(importKeyButton, 2, 0);
+
+        Button balanceButton = new Button("查询余额");
+        balanceButton.getStyleClass().add("button-raised");
+        buttonBox.add(balanceButton, 3, 0);
+
+        Button stopButton = new Button("停止");
+        stopButton.getStyleClass().add("button-raised");
+        stopButton.setDisable(true); // 刚开始停止按钮不可点击
+        buttonBox.add(stopButton, 4, 0);
+
+
+
+        // 设置日志区域
+        TextArea logArea = new TextArea();
+        logArea.setEditable(false);
+        VBox.setVgrow(logArea, Priority.ALWAYS);
+
+        // 将按钮和日志区域添加到垂直布局中
+        root.getChildren().addAll(buttonBox, logArea);
+
+        // 设置生成私钥的按钮点击事件
+        generateKeyButton.setOnAction(e -> {
+
+            // 选择保存路径
+            DirectoryChooser directoryChooser = new DirectoryChooser();
+            directoryChooser.setTitle("选择保存路径");
+            File selectedDirectory = directoryChooser.showDialog(primaryStage);
+
+            // 如果选择了路径则在后台线程中执行生成私钥的操作
+            if (selectedDirectory != null) {
+                Task<String> task = new Task<String>() {
+                    @Override
+                    protected String call() {
+                        // 生成一个新的以太坊账户
+                        return AccountHelper.generateNewWalletFile(selectedDirectory.getAbsolutePath());
+                    }
+                };
+
+                // 创建一个进度条
+                ProgressBar progressBar = new ProgressBar();
+                VBox.setVgrow(progressBar, Priority.NEVER);
+                root.getChildren().add(0, progressBar);
+
+                // 监听任务状态变化
+                task.setOnSucceeded(event -> {
+                    String result = task.getValue();
+                    if (result != null) {
+                        // 在日志区域显示结果
+                        logArea.appendText("任务执行成功，结果为：" + result + "\n");
+                        // 弹出对话框显示结果
+                        Alert alert = new Alert(Alert.AlertType.INFORMATION, "任务执行成功，结果为：" + result);
+                        alert.showAndWait();
+                    }
+                    // 将进度条移除
+                    root.getChildren().remove(progressBar);
+                });
+                task.setOnFailed(event -> {
+                    // 在日志区域显示错误信息
+                    logArea.appendText("任务执行失败，错误信息：" + task.getException().getMessage() + "\n");
+                    // 将进度条移除
+                    root.getChildren().remove(progressBar);
+                });
+
+                // 启动任务
+                currentThread = new Thread(task);
+                currentThread.setDaemon(true);
+                currentThread.start();
+                // 设置停止按钮可点击
+                stopButton.setDisable(false);
+                stopButton.setOnAction(event -> {
+                    // 中断任务
+                    if (currentThread != null && currentThread.isAlive()) {
+                        currentThread.interrupt();
+                        // 在日志区域显示结果
+                        logArea.appendText("任务已取消\n");
+                        // 将进度条移除
+                        root.getChildren().remove(progressBar);
+                        // 设置停止按钮不可点击
+                        stopButton.setDisable(true);
+                    }
+                });
+            }
+        });
+
+        // 设置导入私钥的按钮点击事件
+        importKeyButton.setOnAction(e -> {
+
+            // 选择私钥文件
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("选择私钥文件");
+            fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("All Files", "*.*"));
+            File selectedFile = fileChooser.showOpenDialog(primaryStage);
+
+            // 如果选择了文件则在后台线程中执行导入私钥的操作
+            if (selectedFile != null) {
+                Task<Credentials> task = new Task<Credentials>() {
+                    @Override
+                    protected Credentials call() {
+                        // 从文件中读取私钥
+                        return AccountHelper.loadCredentials(selectedFile.getAbsolutePath());
+                    }
+                };
+
+                // 创建一个进度条
+                ProgressBar progressBar = new ProgressBar();
+                VBox.setVgrow(progressBar, Priority.NEVER);
+                root.getChildren().add(0, progressBar);
+
+                // 监听任务状态变化
+                task.setOnSucceeded(event -> {
+                    Credentials result = task.getValue();
+                    if (result != null) {
+                        // 在日志区域显示结果
+                        logArea.appendText("任务执行成功，地址为：" + result.getAddress() + "\n");
+                        // 弹出对话框显示结果
+                        Alert alert = new Alert(Alert.AlertType.INFORMATION, "任务执行成功，地址为：" + result.getAddress());
+                        alert.showAndWait();
+                    }
+                    // 将进度条移除
+                    root.getChildren().remove(progressBar);
+                });
+                task.setOnFailed(event -> {
+                    // 在日志区域显示错误信息
+                    logArea.appendText("任务执行失败，错误信息：" + task.getException().getMessage() + "\n");
+                    // 将进度条移除
+                    root.getChildren().remove(progressBar);
+                });
+
+                // 启动任务
+                currentThread = new Thread(task);
+                currentThread.setDaemon(true);
+                currentThread.start();
+                // 设置停止按钮可点击
+                stopButton.setDisable(false);
+                stopButton.setOnAction(event -> {
+                    // 中断任务
+                    if (currentThread != null && currentThread.isAlive()) {
+                        currentThread.interrupt();
+                        // 在日志区域显示结果
+                        logArea.appendText("任务已取消\n");
+                        // 将进度条移除
+                        root.getChildren().remove(progressBar);
+                        // 设置停止按钮不可点击
+                        stopButton.setDisable(true);
+                    }
+                });
+            }
+        });
+
+        // 设置查询余额的按钮点击事件
+        balanceButton.setOnAction(e -> {
+
+            // 选择账户地址
+            TextInputDialog dialog = new TextInputDialog();
+            dialog.setTitle("输入账户地址");
+            dialog.setHeaderText("请输入要查询的账户地址：");
+            dialog.setContentText("地址：");
+            dialog.getDialogPane().lookupButton(ButtonType.CANCEL).setVisible(true);
+            dialog.getDialogPane().lookupButton(ButtonType.CANCEL).addEventFilter(ActionEvent.ACTION, event -> {
+                // 中断任务
+                if (currentThread != null && currentThread.isAlive()) {
+                    currentThread.interrupt();
+                    // 在日志区域显示结果
+                    logArea.appendText("任务已取消\n");
+                    // 设置停止按钮不可点击
+                    stopButton.setDisable(true);
+                }
+            });
+
+            String address = dialog.showAndWait().orElse(null);
+
+            // 如果输入了地址则在后台线程中执行查询余额的操作
+            if (address != null && !address.isEmpty()) {
+                Task<BigDecimal> task = new Task<BigDecimal>() {
+                    @Override
+                    protected BigDecimal call() {
+                        return AccountHelper.getBalance(address);
+                    }
+                };
+                // 创建一个进度条
+                ProgressBar progressBar = new ProgressBar();
+                VBox.setVgrow(progressBar, Priority.NEVER);
+                root.getChildren().add(0, progressBar);
+
+                // 监听任务状态变化
+                task.setOnSucceeded(event -> {
+                    BigDecimal result = task.getValue();
+                    if (result != null) {
+                        // 在日志区域显示结果
+                        logArea.appendText("任务执行成功，余额为：" + Convert.fromWei(result, Convert.Unit.ETHER) + " ETH\n");
+                        // 弹出对话框显示结果
+                        Alert alert = new Alert(Alert.AlertType.INFORMATION,
+                                "任务执行成功，余额为：" + Convert.fromWei(result, Convert.Unit.ETHER) + " ETH");
+                        alert.showAndWait();
+                    }
+                    // 将进度条移除
+                    root.getChildren().remove(progressBar);
+                });
+                task.setOnFailed(event -> {
+                    // 在日志区域显示错误信息
+                    logArea.appendText("任务执行失败，错误信息：" + task.getException().getMessage() + "\n");
+                    // 将进度条移除
+                    root.getChildren().remove(progressBar);
+                    // 设置停止按钮不可点击
+                    stopButton.setDisable(true);
+                });
+
+                // 启动任务
+                currentThread = new Thread(task);
+                currentThread.setDaemon(true);
+                currentThread.start();
+                // 设置停止按钮可点击
+                stopButton.setDisable(false);
+                stopButton.setOnAction(event -> {
+                    // 中断任务
+                    if (currentThread != null && currentThread.isAlive()) {
+                        currentThread.interrupt();
+                        // 在日志区域显示结果
+                        logArea.appendText("任务已取消\n");
+                        // 将进度条移除
+                        root.getChildren().remove(progressBar);
+                        // 设置停止按钮不可点击
+                        stopButton.setDisable(true);
+                    }
+                });
+            }
+        });
+
+        // 新建一个ScheduledService
+        ScheduledService<BigDecimal> gasPriceService = new ScheduledService<BigDecimal>() {
+            @Override
+            protected Task<BigDecimal> createTask() {
+                return new Task<BigDecimal>() {
+                    @Override
+                    protected BigDecimal call() {
+                        return AccountHelper.getGasPrice();
+                    }
+                };
+            }
+        };
+
+        // 每5秒更新一次GAS费用
+        gasPriceService.setPeriod(Duration.seconds(5));
+
+        // 监听服务状态变化
+        gasPriceService.setOnSucceeded(event -> {
+            BigDecimal gasPrice = gasPriceService.getValue();
+            if (gasPrice != null) {
+                // 在日志区域显示结果
+                gasFeeLabel.setText("GAS Fee: " + gasPrice);
+            } else {
+                // 在日志区域显示错误信息
+                gasFeeLabel.setText("查询GAS费用失败\n");
+            }
+        });
+        gasPriceService.setOnFailed(event -> {
+            // 在日志区域显示错误信息
+            gasFeeLabel.setText("查询GAS费用失败\n");
+        });
+
+// 启动服务
+        gasPriceService.start();
+
+        Scene scene = new Scene(root, 800, 600);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("以太坊学习笔记");
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/icon.jpeg")));
+
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/io/github/pleuvoir/web3/utils/AccountHelper.java
@@ -0,0 +1,145 @@
+package io.github.pleuvoir.web3.utils;
+
+import java.math.RoundingMode;
+import jnr.a64asm.PREF_ENUM;
+import lombok.Data;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.web3j.crypto.CipherException;
+import org.web3j.crypto.Credentials;
+import org.web3j.crypto.WalletUtils;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameterName;
+import org.web3j.protocol.core.methods.response.EthBlock;
+import org.web3j.protocol.core.methods.response.EthBlockNumber;
+import org.web3j.protocol.core.methods.response.EthGasPrice;
+import org.web3j.protocol.core.methods.response.EthGetBalance;
+import org.web3j.protocol.http.HttpService;
+import org.web3j.utils.Convert;
+
+import java.io.File;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.sql.SQLOutput;
+import org.web3j.utils.Convert.Unit;
+
+@Slf4j
+public class AccountHelper {
+
+    static Web3j web3j;
+
+    static {
+        web3j = Web3j.build(new HttpService("https://mainnet.infura.io/v3/?"));
+    }
+
+    @Data
+    static class AccountDTO {
+        private String privateKey;
+        private String publicKey;
+        private String address;
+    }
+
+    public static AccountDTO generateFullNewWalletFile(String filePath) {
+        try {
+            String wallet = WalletUtils.generateFullNewWalletFile("9527", new File(filePath));
+            AccountDTO accountDTO = new AccountDTO();
+            accountDTO.setPrivateKey(wallet.substring(0, 64));
+            accountDTO.setPublicKey(wallet.substring(64, 128));
+            accountDTO.setAddress(wallet.substring(128));
+            return accountDTO;
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 写入本地账户，返回钱包地址
+     */
+    public static String generateNewWalletFile(String directoryPath) {
+        try {
+            String walletFileName = WalletUtils.generateNewWalletFile(
+                    "9527", // 设置一个密码用于加密私钥
+                    new File(directoryPath), true); // 指定保存账户信息的目录
+
+            String oldFileName = directoryPath + "/" + walletFileName;
+            Credentials credentials = loadCredentials(oldFileName);
+            String address = credentials.getAddress();
+
+            File oldFile = new File(oldFileName);
+            File newFile = new File(directoryPath + "/" + address);
+            if (oldFile.renameTo(newFile)) {
+                System.out.println("文件重命名成功。");
+            } else {
+                System.out.println("文件重命名失败。");
+            }
+            return address;
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 根据keyStore加载账户
+     */
+    public static Credentials loadCredentials(String filePath) {
+        try {
+            return WalletUtils.loadCredentials("9527", new File(filePath));
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public static BigDecimal getBalance(String address) {
+        try {
+            log.info("获取以太坊账户余额 {}",address);
+            EthGetBalance balance = web3j.ethGetBalance(address, DefaultBlockParameterName.LATEST).send();
+            return new BigDecimal(balance.getBalance());
+        } catch (IOException e) {
+            log.error("",e);
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 获取以太坊的最新 GAS 费用
+     */
+    public static BigDecimal getGasPrice() {
+        try {
+            BigInteger gasPrice = web3j.ethGasPrice().send().getGasPrice();
+           return Convert.fromWei(String.valueOf(gasPrice), Unit.GWEI).setScale(0, RoundingMode.HALF_UP);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+    public static void main(String[] args) throws IOException {
+        BigDecimal gasPrice = getGasPrice();
+
+        System.out.println(gasPrice);
+
+        // 将 gas price 转换为 Gwei
+        BigDecimal bigDecimal = Convert.fromWei(String.valueOf(gasPrice), Unit.GWEI).setScale(2, RoundingMode.HALF_UP);
+
+        System.out.println(bigDecimal + " Gwei");
+
+//        BigDecimal bigDecimal = Convert.toWei(gasPrice.toString(), Convert.Unit.GWEI);
+//
+//
+//        System.out.println(bigDecimal);
+//
+//        EthGetBalance send = web3j.ethGetBalance("0x6f7a5b92f38961355c331ffe1dec145886f28b24", DefaultBlockParameterName.LATEST)
+//                .send();
+//        System.out.println(send.getBalance());
+//
+//        BigDecimal balance = getBalance("0x6f7a5b92f38961355c331ffe1dec145886f28b24");
+//        System.out.println(balance);
+    }
+}
